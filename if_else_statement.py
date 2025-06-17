a541ea9a174a47ec704c9499a68c094d0db2da4f
unknown <ainarasolofoniaina1@gmail.com>
2025-06-17 19:32:38 -0400

    If else statement soccer context

---
+++ if_else_statement.py
@@ -0,0 +1,10 @@
+# Let's decide what to do with the ball!
+
+has_clear_shot = input("Do you have a clear shot at the goal? (yes/no): ")
+
+if has_clear_shot == "yes":
+    print("SHOOT! Go for the goal!")
+else:
+    print("Pass the ball to a teammate!")
+
+print("Great play!")
